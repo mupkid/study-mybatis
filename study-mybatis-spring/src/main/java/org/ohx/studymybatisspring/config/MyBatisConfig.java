6fe742387321f445be22693639fd53c5f2f9d8e6
ohx <mupkid123456@gmail.com>
2022-09-11 15:42:48 +0800

    Spring 项目整合 MyBatis

--- study-mybatis-spring/src/main/java/org/ohx/studymybatisspring/config/MyBatisConfig.java
+++ study-mybatis-spring/src/main/java/org/ohx/studymybatisspring/config/MyBatisConfig.java
@@ -4,12 +4,19 @@ import org.apache.ibatis.logging.slf4j.Slf4jImpl;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.annotation.MapperScannerRegistrar;
+import org.mybatis.spring.mapper.MapperScannerConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
 
 /**
+ * 相当于 MyBatis 的 XML 配置文件
+ * <p>
+ * 使用{@link MapperScan}扫描 Mapper 包获得所有 Mapper，底层与{@link MapperScannerRegistrar}有关，所以也可以自定义
+ * {@link MapperScannerConfigurer}来自定义规则。
+ *
  * @author mudkip
  * @date 2022/9/11
  */
